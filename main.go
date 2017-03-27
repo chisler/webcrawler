c273bb451588fa6ab1b414ddb27682f5dbc891c8
chisler <alexander.chisler@gmail.com>
2017-03-27 15:12:14 +0300

    Printing to file/stdout the map -> function.

--- main.go
+++ main.go
@@ -1,52 +1,70 @@
 package main
 
 import (
-	"log"
-	"time"
-	"fmt"
 	"./crawl"
 	"./fetch"
-	"os"
 	"flag"
+	"fmt"
+	"log"
+	"os"
+	"time"
+	"syscall"
 )
 
 func cmd_usage() {
 	fmt.Fprintf(os.Stderr, "Usage: main http://example.com/\n")
-	flag.PrintDefaults()
 	os.Exit(2)
 }
 
-func main() {
-	flag.Parse()
+func printResultMap(crawler *crawl.WebCrawler, start time.Time, end time.Duration) {
+	f, err := os.Create("result.txt")
 
+	if err != nil {
+		log.Panic(err)
+		log.Println("Result will be printed to stdout.")
+		f = os.NewFile(uintptr(syscall.Stdout), "/dev/stdout")
+	}
+
+	f.WriteString("_________MAP__________\n\n")
+
+	f.WriteString(fmt.Sprintf("Extracted %v pages\n", len(crawler.Fetched)))
+	f.WriteString(fmt.Sprintf("Extracted %v pages\n", len(crawler.Fetched)))
+	f.WriteString(fmt.Sprintf("Execution started at %v\n", start))
+	f.WriteString(fmt.Sprintf("Execution took %v\n\n", end))
+
+	for k, v := range crawler.Fetched {
+		f.WriteString(fmt.Sprintf("Node: %v \nUrls: %v \nAssets: %v \n\n", k, v.Urls, v.Assets))
+	}
+}
+
+func main() {
+	//Receive command line arguments
+	flag.Parse()
 	args := flag.Args()
 	fmt.Println(args)
+
 	if len(args) < 1 {
 		cmd_usage()
 		fmt.Println("Please specify start page")
 		os.Exit(1)
 	}
-
 	startUrl := args[0]
 
+	//Initialize a crawler
 	crawler := crawl.NewWebCrawler()
+
+	//Initialize a fetcher
 	fetcher, err := fetch.NewPageFetcher(startUrl)
-
-
 	if err != nil {
 		log.Fatal("Failed to parse starting URL: %s", startUrl)
 		return
 	}
 
+	//Crawl pages and
 	start := time.Now()
-	crawler.Crawl(startUrl, 3, fetcher)
+	crawler.Crawl(startUrl, 1, fetcher)
 	end := time.Since(start)
 
-	fmt.Println("_________MAP__________")
-	for k, v := range crawler.Fetched {
-		fmt.Printf("Node: %v \nUrls: %v \nAssets: %v \n\n", k, v.Urls, v.Assets)
-	}
-	fmt.Printf("Extracted %v pages\n", len(crawler.Fetched))
-
-	fmt.Printf("Execution took %v", end)
+	//Show results
+	printResultMap(crawler, start, end)
 }
