9e3e1ce48f64b757b50750012a8f5a650d586062
chisler <alexander.chisler@gmail.com>
2017-03-27 10:42:58 +0300

    Static assets can be extracted and need to be filtered.

--- main.go
+++ main.go
@@ -8,6 +8,31 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+
+
+func printStaticAssets(urlString string) {
+	doc, err := goquery.NewDocument(urlString)
+	if err != nil {
+		log.Fatal(err)
+
+	}
+
+	doc.Find("script").Each(func(index int, linkTag *goquery.Selection) {
+		link, _ := linkTag.Attr("src")
+		fmt.Printf("Script #%d:'%s'\n", index, link)
+	})
+
+	doc.Find("img").Each(func(index int, linkTag *goquery.Selection) {
+		link, _ := linkTag.Attr("src")
+		fmt.Printf("Img #%d:'%s'\n", index, link)
+	})
+
+	doc.Find("link").Each(func(index int, linkTag *goquery.Selection) {
+		link, _ := linkTag.Attr("href")
+		fmt.Printf("Link #%d:'%s'\n", index, link)
+	})
+}
+
 func printLinks(urlString string) {
 	doc, err := goquery.NewDocument(urlString)
 	if err != nil {
@@ -17,11 +42,11 @@ func printLinks(urlString string) {
 
 	doc.Find("a").Each(func(index int, linkTag *goquery.Selection) {
 		link, _ := linkTag.Attr("href")
-		fmt.Printf("Link #%d:'%s'\n", index, link)
+		fmt.Printf("A #%d:'%s'\n", index, link)
 	})
-	return
 }
 
 func main() {
-	printLinks("http://example.com")
+	printLinks("https://monzo.com/")
+	printStaticAssets("https://monzo.com/")
 }
