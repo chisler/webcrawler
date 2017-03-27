1581188009babcf4d52b1d7f218f0ed2d06bedb2
chisler <alexander.chisler@gmail.com>
2017-03-27 11:19:36 +0300

    External links can be filtered out. Url normalization and filtering needs testing.

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-
 import (
 	"fmt"
 	"log"
@@ -17,24 +16,23 @@ var linkTagAllowedTypes = map[string]bool{
 	"shortcut icon": true,
 }
 
-
-func Fetch(urlString string)  {
+func Fetch(urlString string) {
 	doc, err := goquery.NewDocument(urlString)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(getLinks(doc))
+	fmt.Println(getInternalLinks(doc))
 	fmt.Println(getStaticAssets(doc))
 }
 
 func getStaticAssets(doc *goquery.Document) (res []*url.URL) {
 
 	//Add <script> tag assets
-	res = getAttrsFromTags(doc, "script", "src")
+	res = getUrlsFromTags(doc, "script", "src")
 
 	//Add <img> tag assets
-	res = append(res, getAttrsFromTags(doc, "img", "src")...)
+	res = append(res, getUrlsFromTags(doc, "img", "src")...)
 
 	//Add <link> tag assets
 	doc.Find("link").Each(func(_ int, linkTag *goquery.Selection) {
@@ -51,13 +49,24 @@ func getStaticAssets(doc *goquery.Document) (res []*url.URL) {
 	return
 }
 
-func getLinks(doc *goquery.Document) (res []*url.URL) {
-	res = getAttrsFromTags(doc, "a", "href")
+//Returns all links limited to one domain of startUrl
+func getInternalLinks(doc *goquery.Document) (res []*url.URL) {
+
+	allLinks := getAllLinks(doc)
+	res = excludeExternalLinks(allLinks, doc.Url)
+
+	return
+}
+
+//Returns all links of the page
+func getAllLinks(doc *goquery.Document) (res []*url.URL) {
+
+	res = getUrlsFromTags(doc, "a", "href")
 	return
 }
 
 //Returns attrs from document by tag and attr
-func getAttrsFromTags(doc *goquery.Document, tagName, attrName string) (res []*url.URL) {
+func getUrlsFromTags(doc *goquery.Document, tagName, attrName string) (res []*url.URL) {
 
 	doc.Find(tagName).Each(func(index int, linkTag *goquery.Selection) {
 		if urlAttr, ok := linkTag.Attr(attrName); ok && urlAttr != "" {
@@ -66,6 +75,18 @@ func getAttrsFromTags(doc *goquery.Document, tagName, attrName string) (res []*u
 			}
 		}
 	})
+	return
+}
+
+func excludeExternalLinks(urls []*url.URL, host *url.URL) (filteredLinks []*url.URL) {
+
+	filteredLinks = urls[:0]
+	for _, current_url := range urls {
+		if host.Host == current_url.Host {
+			filteredLinks = append(filteredLinks, current_url)
+		}
+	}
+
 	return
 }
 
@@ -78,7 +99,6 @@ func normalizeUrl(urlString string, host *url.URL) (normalizedUrl *url.URL) {
 
 	return
 }
-
 
 func main() {
 	Fetch("https://monzo.com/")
