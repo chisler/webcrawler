67b3a88e3dac270aafdc953aa0240479ab8f3c52
chisler <alexander.chisler@gmail.com>
2017-03-27 13:09:56 +0300

    Crawler interface added, WebCrawler constructor for instance that uses Crawler. Comments for crawler interface added.

--- main.go
+++ main.go
@@ -1,81 +1,107 @@
 package main
 
 import (
-	"sync"
-	"net/url"
-	"./fetch"
-	"log"
-	"time"
 	"fmt"
+	"log"
+	"net/url"
+	"sync"
+	"time"
+	"./fetch"
 )
 
-type Storage struct {
-	fetched map[string][]*url.URL
-	sync.RWMutex
+type Crawler interface {
+	// Crawl uses fetcher to recursively and asynchronously crawl
+	// pages starting with url, to a maximum of depth.
+	Crawl(currentUrl string, depth int, fetcher fetch.Fetcher)
 }
 
-var store Storage
+//WebCrawler instance - abuser of Crawler interface
+type WebCrawler struct {
+	Fetched map[string][]*url.URL
+	sync.RWMutex
+	errors  []error
+}
 
-// Crawl uses fetcher to recursively crawl
-// pages starting with url, to a maximum of depth.
-// Based on golang tour solution
-func Crawl(currentUrl string, depth int, fetcher fetch.Fetcher) {
+//Constructor of WebCrawler struct
+func NewWebCrawler() *WebCrawler {
+
+	return &WebCrawler{
+		Fetched: make(map[string][]*url.URL),
+	}
+
+}
+
+//Recursively crawls pages
+// extracts static assets and URLs
+func (crawler *WebCrawler) Crawl(currentUrl string, depth int, fetcher fetch.Fetcher) {
+
+	//Do not Crawl page if depth was exceeded
 	if depth == 0 {
 		return
 	}
 
-	store.RLock()
-	if _, ok := store.fetched[currentUrl]; ok {
-		store.RUnlock()
+	//Do not Crawl page twice
+	crawler.RLock()
+	if _, ok := crawler.Fetched[currentUrl]; ok {
+		crawler.RUnlock()
 		return
 	}
-	store.RUnlock()
+	crawler.RUnlock()
 
+	//Fetch page's static assets and URLs
 	assets, urls, err := fetcher.Fetch(currentUrl)
-
 	if err != nil {
 		log.Println(err)
+		crawler.Lock()
+		crawler.errors = append(crawler.errors, err)
+		crawler.Unlock()
 		return
 	}
 
-	store.Lock()
-	store.fetched[currentUrl] = assets
-	store.Unlock()
+	//Add fetched resources to a storage synchronously
+	crawler.Lock()
+	crawler.Fetched[currentUrl] = assets
+	crawler.Unlock()
 
+	//Recursively and asynchronously Crawl URLs just fetched
 	done := make(chan bool)
 	for _, u := range urls {
 		go func(currentUrl *url.URL) {
-			Crawl(currentUrl.String(), depth-1, fetcher)
+			crawler.Crawl(currentUrl.String(), depth-1, fetcher)
 			done <- true
 		}(u)
 	}
 
+	//Wait for deeper Crawls to end
 	for i := 0; i < len(urls); i++ {
 		<-done
 	}
+
 	return
 }
 
 func main() {
 	startUrl := "http://jeniasofronov.ru/"
+	crawler := NewWebCrawler()
 	fetcher, err := fetch.NewPageFetcher(startUrl)
+
 
 	if err != nil {
 		log.Fatal("Failed to parse starting URL: %s", startUrl)
 		return
 	}
 
-	store.fetched = make(map[string][]*url.URL)
 
 	start := time.Now()
 
-	Crawl(startUrl, 3, fetcher)
-
+	crawler.Crawl(startUrl, 3, fetcher)
 	fmt.Println("_________MAP__________")
-	for k, v := range store.fetched {
+	for k, v := range crawler.Fetched {
 		fmt.Printf("Node: %v \nAssets: %v \n\n", k, v)
 	}
+	fmt.Printf("Extracted %v pages\n", len(crawler.Fetched))
+
 	end := time.Since(start)
-	fmt.Printf("Extracted %v pages\n", len(store.fetched))
 	fmt.Printf("Execution took %v", end)
+
 }
