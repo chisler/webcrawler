1bd9ee7c65d3813aa0b3020a186db92bcdbfb509
chisler <alexander.chisler@gmail.com>
2017-03-27 11:26:33 +0300

    Test exclude external links from all links fetched.

--- main_test.go
+++ main_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 	"github.com/stretchr/testify/assert"
 	"net/url"
+	"github.com/PuerkitoBio/goquery"
+	"strings"
 )
 
 func TestNormalizeUrl(t *testing.T) {
@@ -22,4 +24,18 @@ func TestNormalizeUrl(t *testing.T) {
 		assert.Equal(t, tt.expected, actual)
 	}
 
+}
+
+func TestExcludeExternalLinks(t *testing.T) {
+	host, _ := url.Parse("http://example.com/")
+	var html = `
+		<a href="http://example.com/about"></a>			 OK
+		<a href="http://facebook.com/"></a>			 NOT OK
+		`
+	doc, _ := goquery.NewDocumentFromReader(strings.NewReader(html))
+	allLinks := getAllLinks(doc)
+	filteredLinks := excludeExternalLinks(allLinks, host)
+
+	assert.Equal(t, "http://example.com/about", filteredLinks[0].String())
+	assert.Equal(t, 1, len(filteredLinks))
 }
