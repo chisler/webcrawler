7b63869b7ed98a210c6c785b89b48b5a8d5cb1f4
chisler <alexander.chisler@gmail.com>
2017-03-27 14:48:37 +0300

    Starting URL as command line argument.

--- main.go
+++ main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
-	"./crawl"
-	"./fetch"
-	"fmt"
 	"log"
 	"time"
+	"fmt"
+	"./crawl"
+	"./fetch"
+	"os"
+	"flag"
 )
 
+func cmd_usage() {
+	fmt.Fprintf(os.Stderr, "Usage: main http://example.com/\n")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
 func main() {
-	startUrl := "http://jeniasofronov.ru/"
+	flag.Parse()
+
+	args := flag.Args()
+	fmt.Println(args)
+	if len(args) < 1 {
+		cmd_usage()
+		fmt.Println("Please specify start page")
+		os.Exit(1)
+	}
+
+	startUrl := args[0]
 
 	crawler := crawl.NewWebCrawler()
 	fetcher, err := fetch.NewPageFetcher(startUrl)
+
 
 	if err != nil {
 		log.Fatal("Failed to parse starting URL: %s", startUrl)
@@ -20,9 +39,8 @@ func main() {
 	}
 
 	start := time.Now()
-
 	crawler.Crawl(startUrl, 3, fetcher)
-
+	end := time.Since(start)
 
 	fmt.Println("_________MAP__________")
 	for k, v := range crawler.Fetched {
@@ -30,6 +48,5 @@ func main() {
 	}
 	fmt.Printf("Extracted %v pages\n", len(crawler.Fetched))
 
-	end := time.Since(start)
 	fmt.Printf("Execution took %v", end)
 }
